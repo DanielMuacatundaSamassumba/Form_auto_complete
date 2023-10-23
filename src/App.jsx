696d3375766d18243e84f5ade0659467a8115d7b
DanielMuacatundaSamassumba <danielmuacatunda@gmail.com>
2023-10-23 11:17:09 +0100

    Finish

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,24 @@ import './App.css'
 
 function App() {
   const [Bi, setBi]=  useState("")
+   const [name,setName]=useState("")
+   const [Birth,setBirth]=useState("")
+   const [Place,setPlace]=useState("")
 const handlebi =async(e)=>{
 
-  if(e.target.value.length == 14){
-    setBi(e.target.value)
-    await fetch(`https://consulta.edgarsingui.ao/consultar/${Bi}`).then((response)=>{
-   console.log(response.json())
+
+
+  await fetch(`https://consulta.edgarsingui.ao/consultar/${e.target.value}`).then((response)=>{
+response.json().then((res)=>{
+setName(res.name)
+setBirth(res.data_de_nascimento)
+setPlace(res.morada)
+
+})
   
   })
 
-  }
+  
 }
 
   return (
@@ -24,11 +32,11 @@ const handlebi =async(e)=>{
       <div className="content">
         <div className="content-input">
           <input type="text" placeholder='Nº BI' onChange={handlebi} />
-          <input type="text" placeholder='Nome Completo' disabled/>
+          <input type="text" placeholder='Nome Completo' disabled value={name}/>
         </div>
         <div className="content-input">
-          <input type="text" placeholder='Data de Nascimento' disabled/>
-          <input type="text"  placeholder='Morada' disabled/>
+          <input type="text" placeholder='Data de Nascimento' disabled value={Birth}/>
+          <input type="text"  placeholder='Morada' disabled value={Place}/>
         </div>
         <div className="btn">
           <button>Cadastrar</button>
